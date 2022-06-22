00f4a2334db77b788a011d41299b875fe143d1aa
jaeyoung <jy.choi@chopsticks.market>
2022-06-22 20:25:52 +0900

    세팅: 아폴로 클라이언트 환경 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 import App from "./App";
+
+const client = new ApolloClient({
+  uri: "http://13.124.104.219:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
