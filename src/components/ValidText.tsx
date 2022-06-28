56344a5a548885b38fc954b52d7dd4e35b373a16
jaeyoung <jy.choi@chopsticks.market>
2022-06-28 18:18:30 +0900

    추가: ValidText 컴포넌트 추가

---
+++ src/components/ValidText.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const ValidText = styled.p<{ valid: boolean }>`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 14px;
+  letter-spacing: 0.1px;
+
+  color: ${({ valid, theme: { palette } }) =>
+    valid ? palette.black : palette.red900};
+
+  margin-bottom: -14px;
+
+  span.red-text {
+    color: ${({ theme: { palette } }) => palette.red900};
+  }
+`;
+
+export default ValidText;
