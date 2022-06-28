c2853e8e1ed23809842b44c9ff270270ae7e949c
jaeyoung <jy.choi@chopsticks.market>
2022-06-28 15:04:17 +0900

    추가: EnterPreneurInfo UI 변경시 적용되는 정적 컴포넌트 추가

--- src/components/ShopSetting/EnterPreneurInfo.tsx
+++ src/components/ShopSetting/EnterPreneurInfo.tsx
@@ -8,12 +8,42 @@ const EnterPreneurInfo = () => {
       <SubTitleWrapper>
         <SubTitle>사업자 정보(간이,법인)</SubTitle>
       </SubTitleWrapper>
-      <PreneurInfoContainer>
+      {/* <HasNoInfoContainer>
         <InfoText>등록된 사업자등록증/통신판매업신고증이 없습니다.</InfoText>
         <Button size="small" full={false}>
           등록하기
         </Button>
-      </PreneurInfoContainer>
+      </HasNoInfoContainer> */}
+      <HasInfoContainer>
+        <EntrePreneurInfoList>
+          <EntrepreneurInfoContainer>
+            <EntrePreneurText>대표자명</EntrePreneurText>
+            <EntrePreneurText>김지원</EntrePreneurText>
+          </EntrepreneurInfoContainer>
+          <EntrepreneurInfoContainer>
+            <EntrePreneurText>사업자등록번호 </EntrePreneurText>
+            <EntrePreneurText>123-45-12345</EntrePreneurText>
+          </EntrepreneurInfoContainer>
+          <EntrepreneurInfoContainer>
+            <EntrePreneurText>법인등록번호</EntrePreneurText>
+            <EntrePreneurText>해당사항없음</EntrePreneurText>
+          </EntrepreneurInfoContainer>
+          <EntrepreneurInfoContainer>
+            <EntrePreneurText>간이과세대상자</EntrePreneurText>
+            <EntrePreneurText>대상</EntrePreneurText>
+          </EntrepreneurInfoContainer>
+          <EntrepreneurInfoContainer>
+            <EntrePreneurText>소재지</EntrePreneurText>
+            <EntrePreneurText>
+              서울특별시 종로구 새문안로 3길 3, 내일신문빌딩 5층
+            </EntrePreneurText>
+          </EntrepreneurInfoContainer>
+          <EntrepreneurInfoContainer>
+            <EntrePreneurText>통신판매업신고번호</EntrePreneurText>
+            <EntrePreneurText>2020-서울송파-3260</EntrePreneurText>
+          </EntrepreneurInfoContainer>
+        </EntrePreneurInfoList>
+      </HasInfoContainer>
     </Container>
   );
 };
@@ -37,7 +67,7 @@ const SubTitle = styled.h2`
   line-height: 20px;
 `;
 
-const PreneurInfoContainer = styled.div`
+const HasNoInfoContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -57,6 +87,37 @@ const PreneurInfoContainer = styled.div`
     text-align: center;
     letter-spacing: 0.1px;
   }
+`;
+
+const HasInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  min-width: 736px;
+  padding: 24px 40px;
+  background-color: ${({ theme: { palette } }) => palette.grey100};
+`;
+
+const EntrePreneurInfoList = styled.ul`
+  display: flex;
+  flex-direction: column;
+`;
+const EntrepreneurInfoContainer = styled.li`
+  display: flex;
+  padding-bottom: 8px;
+  margin-bottom: 8px;
+  border-bottom: 1px solid ${({ theme: { palette } }) => palette.grey500};
+
+  & > :first-child {
+    width: 170px;
+  }
+`;
+
+const EntrePreneurText = styled.span`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 18px;
+  letter-spacing: -0.03em;
 `;
 
 const InfoText = styled.h3`
