4d58f9ff57b5f627a7283add1032d95dec547429
jaeyougchoi <jy.choi@chopsticks.market>
2022-10-18 17:20:58 +0900

    추가: 페이지 네이션 스타일링 완료

--- src/components/ProductRegistration/ProductManagement/Pagination.tsx
+++ src/components/ProductRegistration/ProductManagement/Pagination.tsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import mediumDoubleLeftActiveSvg from "@icons/medium-double-left-active.svg";
 import mediumDoubleRightActiveSvg from "@icons/medium-double-right-active.svg";
 import mediumLeftActvieSvg from "@icons/medium-left-active.svg";
 import mediumRightActiveSvg from "@icons/medium-right-active.svg";
+
+import mediumDoubleLeftInActiveSvg from "@icons/medium-double-left-inactive.svg";
+import mediumDoubleRightInActiveSvg from "@icons/medium-double-right-inactive.svg";
+import mediumLeftInActvieSvg from "@icons/medium-left-inactive.svg";
+import mediumRightInActiveSvg from "@icons/medium-right-inactive.svg";
+
 import { useReactiveVar } from "@apollo/client";
 import {
   filterOptionPageNumberVar,
@@ -13,18 +19,17 @@ import {
 const Pagination = () => {
   const filterOptionPageNumber = useReactiveVar(filterOptionPageNumberVar);
   const pageNumberList = useReactiveVar(pageNumberListVar);
-
   const [pageList, setPageList] = useState<Array<number>>(
     pageNumberList.slice(0 * 10, (0 + 1) * 10)
   );
   const [skip, setSkipPage] = useState<number>(0);
+  const totalSkipPage: number = Math.floor(pageNumberList.length / 10);
 
   const handlePageNumberClick = (pageNumber: number) => () => {
     filterOptionPageNumberVar(pageNumber);
   };
 
   const handleNextPageClick = () => {
-    const totalSkipPage: number = Math.floor(pageNumberList.length / 10);
     if (skip >= totalSkipPage) return;
 
     setSkipPage((prev) => prev + 1);
@@ -44,7 +49,6 @@ const Pagination = () => {
   };
 
   const handleEndPageClick = () => {
-    const totalSkipPage: number = Math.floor(pageNumberList.length / 10);
     setSkipPage(totalSkipPage);
     filterOptionPageNumberVar(pageNumberList.length);
   };
@@ -56,11 +60,16 @@ const Pagination = () => {
 
   return (
     <Container>
-      <SkipButton
-        src={mediumDoubleLeftActiveSvg}
+      <DoubleLeftButton
+        isActive={skip > 0}
+        disabled={skip === 0}
         onClick={handleStartPageClick}
       />
-      <SkipButton src={mediumLeftActvieSvg} onClick={handlePrevPageClick} />
+      <SingleLeftButton
+        isActive={skip > 0}
+        disabled={skip === 0}
+        onClick={handlePrevPageClick}
+      />
       <PageNumberList>
         {pageList.map((page, index) => (
           <PageItem
@@ -72,9 +81,15 @@ const Pagination = () => {
           </PageItem>
         ))}
       </PageNumberList>
-      <SkipButton src={mediumRightActiveSvg} onClick={handleNextPageClick} />
-      <SkipButton
-        src={mediumDoubleRightActiveSvg}
+      <SingleRightButton
+        isActive={totalSkipPage !== skip}
+        disabled={totalSkipPage === skip}
+        onClick={handleNextPageClick}
+      />
+
+      <DoubleRightButton
+        isActive={totalSkipPage !== skip}
+        disabled={totalSkipPage === skip}
         onClick={handleEndPageClick}
       />
     </Container>
@@ -82,30 +97,110 @@ const Pagination = () => {
 };
 
 const Container = styled.div`
-  width: 100%;
   display: flex;
-  background-color: #fff;
+  justify-content: center;
+  align-items: center;
+
+  padding: 48px 0px;
+  width: 100%;
 `;
 
-const SkipButton = styled.img`
-  width: 50px;
-  height: 50px;
+const DoubleLeftButton = styled.button<{ isActive: boolean }>`
+  width: 24px;
+  height: 24px;
+
+  background-image: url(${mediumDoubleLeftInActiveSvg});
+  background-repeat: no-repeat;
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      background-image: url(${mediumDoubleLeftActiveSvg});
+    `}
+`;
+
+const SingleLeftButton = styled.button<{ isActive: boolean }>`
+  width: 24px;
+  height: 24px;
+
+  background-image: url(${mediumLeftInActvieSvg});
+  background-repeat: no-repeat;
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      background-image: url(${mediumLeftActvieSvg});
+    `}
+`;
+
+const SingleRightButton = styled.button<{ isActive: boolean }>`
+  width: 24px;
+  height: 24px;
+
+  background-image: url(${mediumRightInActiveSvg});
+  background-repeat: no-repeat;
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      background-image: url(${mediumRightActiveSvg});
+    `}
+`;
+
+const DoubleRightButton = styled.button<{ isActive: boolean }>`
+  width: 24px;
+  height: 24px;
+
+  background-image: url(${mediumDoubleRightInActiveSvg});
+  background-repeat: no-repeat;
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      background-image: url(${mediumDoubleRightActiveSvg});
+    `}
 `;
 
 const PageNumberList = styled.ul`
-  width: 100%;
   display: flex;
+  align-items: flex-start;
+  gap: 16px;
+
+  padding: 0 15px;
 `;
 
 const PageItem = styled.li<{ isActive: boolean }>`
-  margin: 0 5px;
+  position: relative;
 
-  padding: 0.3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-  background-color: ${({ theme: { palette }, isActive }) =>
-    isActive ? palette.red500 : palette.grey500};
+  width: 24px;
+  height: 24px;
 
-  color: #fff;
+  ${({ theme: { palette }, isActive }) =>
+    isActive &&
+    css`
+      &::after {
+        content: "";
+        position: absolute;
+        bottom: 5px;
+
+        display: inline-block;
+        width: 16px;
+        height: 1px;
+        background-color: ${palette.grey500};
+      }
+    `};
+
+  color: #000;
+  font-family: "Spoqa Han Sans Neo";
+  font-size: 13px;
+  font-weight: 400;
+  line-height: 18px;
+  letter-spacing: 0.10000000149011612px;
+  text-align: center;
 
   cursor: pointer;
 `;
