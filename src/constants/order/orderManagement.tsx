0f61b9a6dcceffa67a7197f75ed85851e4501131
jaeyougchoi <jy.choi@chopsticks.market>
2022-11-15 17:03:08 +0900

    추가: 데이터 상수화

--- src/constants/order/orderManagement.tsx
+++ src/constants/order/orderManagement.tsx
@@ -1,5 +1,39 @@
-const orderManagement = () => {
-  return console.log("orderManagement");
+export const ORDER_MANAGEMENT = "주문관리";
+
+export const controllerType = {
+  CONFIRM_ORDER: "주문 확인",
+  SHIPMENT_PROCESSING: "발송 처리",
+  CANCEL_ORDER: "주문 취소",
+  RETURN_PROCESSING: "반품 처리",
+  EXCHANGE_PRODCESSING: "교환 처리",
 };
 
-export default orderManagement;
+export const fixTableType = {
+  MERCHANTITEM_UID: "주문번호",
+  PRODUCT_CODE: "상품 주문번호",
+  ORDER_PRODUCT: "주문 상품",
+  BUYER_NAME: "구매자명",
+  ORDER_STATUS: "주문 상태",
+};
+
+export const scrollTableType = {
+  claimStatus: "클레임 상태",
+  OrderStatus: "주문 상태",
+  COURIER: "택배사",
+  INVOICE_NUMBER: "운송장번호",
+  PAYMENT_DAY: "결제일",
+  RECIPIENT_NAME: "수취인",
+  RECIPIENT_PHONE_NUMBER: "수취인 전화번호",
+  RECIPIENT_ADDRESS: "수취인 주소",
+  POST_CODE: "우편 번호",
+  SHIPMENT_MEMO: "배송 메세지",
+  BUYER_ID: "구매자 아이디",
+  BUYER_PHONE_NUMBER: "구매자 전화번호",
+  OPTION: "옵션",
+  PRODUCT_QUANTITY: "상품개수",
+  PRODUCT_PRICE: "상품가",
+  OPTION_PRICE: "옵션가",
+  PRODUCT_TOTAL_PRICE: "상품별 총금액",
+  SHIPMENT_PRICE: "배송비",
+  ADDITIONAL_SHIPMENT_PRICE: "제주/도서 추가배송비",
+};
