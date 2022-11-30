09af6bf79adf2efb031ac73bc9dbbf78b43039c7
jaeyougchoi <jy.choi@chopsticks.market>
2022-11-30 11:11:33 +0900

    추가: unMount 시에 기존 컴포넌트에 남아있는 상태 초기화

--- src/components/productRegistration/productManagement/ProductTable.tsx
+++ src/components/productRegistration/productManagement/ProductTable.tsx
@@ -260,6 +260,15 @@ const ProductTable = () => {
     setIsCheckedList(checkedList);
     checkedProductIdsVar([]);
     checkAllBoxStatusVar(false);
+
+    return () => {
+      setProducts([]);
+      setIsCheckedList({});
+      commonFilterOptionVar({ page: 1, skip: 20, query: "" });
+      filterOptionVar({ status: null });
+      checkedProductIdsVar([]);
+      checkAllBoxStatusVar(false);
+    };
   }, [data]);
 
   useEffect(() => {
