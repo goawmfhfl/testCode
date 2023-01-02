f1dbee91251308e2b80887cfc639150a6496aa11
justindglee <107393984+justin-chopsticks@users.noreply.github.com>
2023-01-02 10:32:15 +0900

    수정: 샵 소개글에 옵셔널 체이닝 적용

--- src/components/shopSetting/ShopInfo.tsx
+++ src/components/shopSetting/ShopInfo.tsx
@@ -306,7 +306,8 @@ const ShopInfo = () => {
               register={register(SHOP_INTRODUCTION)}
               maxLength={200}
             />
-            <TextCounter>{shopIntroduction.length}/200</TextCounter>
+
+            <TextCounter>{shopIntroduction?.length}/200</TextCounter>
           </TextareaContainer>
         </SectionContainer>
       </ShopInfoContainer>
