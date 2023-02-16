dfbbf4614bd263ae040b9f9a65ceae21819e2945
justindglee <107393984+justin-chopsticks@users.noreply.github.com>
2023-02-17 02:29:06 +0900

    수정: Textarea에 입력된 텍스트값의 대시+스페이스 를 불릿포인트로 변환

--- src/components/common/input/Textarea.tsx
+++ src/components/common/input/Textarea.tsx
@@ -37,7 +37,11 @@ const Textarea = ({
       onKeyDown={onKeyDown}
       placeholder={placeholder}
       maxLength={maxLength}
-      value={value}
+      value={value.replace(/^([-])[\s]$/gm, ($1) => {
+        console.log($1);
+
+        return "• ";
+      })}
     />
   );
 };
